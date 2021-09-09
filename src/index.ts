6861835f9e6fb67c462e8ded504e750ea255820a
Benjamin Thiebaut <benji.thiebaut@gmail.com>
2021-09-09 22:24:49 +0200

    unregistrer shortcuts on window blur

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,11 @@ import fs = require('fs');
 app.disableHardwareAcceleration()
 
 let window: BrowserWindow
-let maps : any = {};
 let ignoreMouseEvent = true;
+let registered = false;
+
+const userConfigFile = path.join(__dirname, '../../../userConfig.json');
+
 function createWindow () {
   window = new BrowserWindow({
     width: 400,
@@ -22,7 +25,7 @@ function createWindow () {
   window.loadFile(path.join(__dirname, '../src/views/overlay.html'));
 
   // NOTE: if you close Dev Tools overlay window will lose transparency 
-  window.webContents.openDevTools({ mode: 'detach', activate: false })
+  //window.webContents.openDevTools({ mode: 'detach', activate: false })
 
   window.setIgnoreMouseEvents(ignoreMouseEvent)
 
@@ -35,61 +38,88 @@ function createWindow () {
   })
 }
 
+function createDefaultUserSc() {
+  let defaultDatas = {
+    'reset'        : 'Shift + 0',
+    'toggleEmf'    : 'Shift + 1',
+    'toggleSpirit' : 'Shift + 2',
+    'toggleFinger' : 'Shift + 3',
+    'toggleOrb'    : 'Shift + 4',
+    'toggleWriting': 'Shift + 5',
+    'toggleTemp'   : 'Shift + 6',
+    'toggleDots'   : 'Shift + 7',
+    'toggleMaps'   : 'm',
+    'exitOverlay'  : 'Shift + Plus',
+  };
+  fs.writeFileSync(userConfigFile, JSON.stringify(defaultDatas, null, 2), {'encoding' : 'utf8'});
+}
+
+function getUserShortcuts(){
+  if (!fs.existsSync(userConfigFile)){
+    createDefaultUserSc();
+  }
+  let userConfigRaw = fs.readFileSync(userConfigFile, {'encoding' : 'utf8'});
+  return JSON.parse(userConfigRaw);
+}
+
+function reset(){
+  window.webContents.send('reset-game', true);
+}
+
+function toggleEmf(){
+  window.webContents.send('toggle-evidence', 'emf');
+}
+function toggleSpirit(){
+  window.webContents.send('toggle-evidence', 'spirit');
+}
+function toggleWriting(){
+  window.webContents.send('toggle-evidence', 'writing');
+}
+function toggleOrb(){
+  window.webContents.send('toggle-evidence', 'orb');
+}
+function toggleFinger(){
+  window.webContents.send('toggle-evidence', 'finger');
+}
+function toggleTemp(){
+  window.webContents.send('toggle-evidence', 'temp');
+}
+function toggleDots(){
+  window.webContents.send('toggle-evidence', 'dots');
+}
+function toggleBone(){
+  window.webContents.send('toggle-bone', true);
+}
+function toggleOuija(){
+  window.webContents.send('toggle-ouija', true);
+}
+function toggleMaps(){
+  window.webContents.send('toggle-maps', true);
+  ignoreMouseEvent = !ignoreMouseEvent;
+  window.setIgnoreMouseEvents(ignoreMouseEvent);
+}
+function exitOverlay(){
+  app.quit(); 
+}
+
 function registerHooks () {
 
-  function reset(){
-    window.webContents.send('reset-game', true);
+  if (!registered){
+
+    globalShortcut.register('Shift + 0', reset);
+    globalShortcut.register('Shift + 1', toggleEmf);
+    globalShortcut.register('Shift + 2', toggleSpirit);
+    globalShortcut.register('Shift + 3', toggleFinger);
+    globalShortcut.register('Shift + 4', toggleOrb);
+    globalShortcut.register('Shift + 5', toggleWriting);
+    globalShortcut.register('Shift + 6', toggleTemp);
+    globalShortcut.register('Shift + 7', toggleDots);
+    globalShortcut.register('Shift + m', toggleMaps);
+    globalShortcut.register('Shift + Plus', exitOverlay);
+
+    registered = true;
   }
 
-  function toggleEmf(){
-    window.webContents.send('toggle-evidence', 'emf');
-  }
-  function toggleSpirit(){
-    window.webContents.send('toggle-evidence', 'spirit');
-  }
-  function toggleWriting(){
-    window.webContents.send('toggle-evidence', 'writing');
-  }
-  function toggleOrb(){
-    window.webContents.send('toggle-evidence', 'orb');
-  }
-  function toggleFinger(){
-    window.webContents.send('toggle-evidence', 'finger');
-  }
-  function toggleTemp(){
-    window.webContents.send('toggle-evidence', 'temp');
-  }
-  function toggleDots(){
-    window.webContents.send('toggle-evidence', 'dots');
-  }
-  function toggleBone(){
-    window.webContents.send('toggle-bone', true);
-  }
-  function toggleOuija(){
-    window.webContents.send('toggle-ouija', true);
-  }
-  function toggleMaps(){
-    window.webContents.send('toggle-maps', true);
-  }
-
-  globalShortcut.register('Shift + 0', reset);
-  globalShortcut.register('Shift + 1', toggleEmf);
-  globalShortcut.register('Shift + 2', toggleSpirit);
-  globalShortcut.register('Shift + 3', toggleFinger);
-  globalShortcut.register('Shift + 4', toggleOrb);
-  globalShortcut.register('Shift + 5', toggleWriting);
-  globalShortcut.register('Shift + 6', toggleTemp);
-  globalShortcut.register('Shift + 7', toggleDots);
-  //globalShortcut.register('Shift + 8', toggleBone);
-  //globalShortcut.register('Shift + 9', toggleOuija);
-
-  globalShortcut.register('Shift + m', () => {
-    toggleMaps();
-    ignoreMouseEvent = !ignoreMouseEvent;
-    window.setIgnoreMouseEvents(ignoreMouseEvent);
-  });
-
-  globalShortcut.register('Shift + Plus', () => { app.quit(); });
 }
 
 function loadSvgMaps(){
@@ -115,10 +145,18 @@ function loadSvgMaps(){
 app.on('ready', createWindow)
 
 app.on('will-quit', () => {
-  // Supprime tous les raccourcis.
   globalShortcut.unregisterAll()
 })
 
 overlayWindow.on('detach', () => {
   app.quit();
+});
+
+overlayWindow.on('blur', () => {
+  globalShortcut.unregisterAll();
+  registered = false;
+});
+
+overlayWindow.on('focus', () => {
+  registerHooks();
 });
